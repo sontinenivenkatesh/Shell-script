7325ae686b98c7267784b66bc1db51ab991c2ca6
venkatesh <ssvenkatesh908@gmail.com>
2023-11-12 10:11:26 +0530

    array

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this called array, instead of single value it can hold multiple vaqlues
+
+persons=("ramesh" "suresh" "sachin")
+
+echo "first person is: ${persons[0]}"
+echo "all persons is: ${persons[@]}
