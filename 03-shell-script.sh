850d98d116d3cc5af909b937858ff47ad97719fe
venkatesh <ssvenkatesh908@gmail.com>
2023-11-12 06:15:35 +0530

    thired one

--- 03-shell-script.sh
+++ 03-shell-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 persion1=$1
 persion2=$2
-echo "persion1 : hi persion2"
-echo "persion2 : hi rersion1"
-echo "persion1 : how is your life"
-echo "persion2 : great and what about you"
-echo "persion1 : going on"
+echo "$persion1 : hi $persion2"
+echo "$persion2 : hi $rersion1"
+echo "$persion1 : how is your life"
+echo "$persion2 : great and what about you"
+echo "$persion1 : going on"
