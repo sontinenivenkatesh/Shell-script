253b361ed6d20e49ce796b1208a775285e5a7c84
venkatesh <ssvenkatesh908@gmail.com>
2023-11-12 06:27:16 +0530

    thired one

--- 03-shell-script.sh
+++ 03-shell-script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 persion1=suresh
 persion2=ramesh
+DATE=$(date)
 echo "$persion1 : hi $persion2"
-echo "$persion2 : hi $rersion1"
+echo "$persion2 : hi $persion1"
 echo "$persion1 : how is your life"
 echo "$persion2 : great and what about you"
 echo "$persion1 : going on"
+echo "Hello, this script is executed at timestamp:$DATE" 
