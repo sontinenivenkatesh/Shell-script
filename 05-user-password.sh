67925694acbe970ce2331f2b4a9f79116164bf74
venkatesh <ssvenkatesh908@gmail.com>
2023-11-12 09:41:39 +0530

    user name and password

---
+++ 05-user-password.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your username"
+read username
+echo "entered username is:$username"
+echo "please enter passoword"
+read password
+echo "entered password is:$password"
