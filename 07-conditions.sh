df9a29c956c94732f15d7dfdd01678a129086479
venkatesh <ssvenkatesh908@gmail.com>
2023-11-13 07:26:40 +0530

    array

---
+++ 07-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# check if the number is greater then 10 or not
+
+number=$1
+
+if[$number -gt 10]
+
+then
+ echo "$number is greater then 10"
+
+else
+ echo "$number is not greater then 10"
+ 
+fi
