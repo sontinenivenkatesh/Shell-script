2b5f357b70f3ffd4f525f014823a1ad1e068712d
venkatesh <ssvenkatesh908@gmail.com>
2023-11-13 08:18:01 +0530

    Install-mySQL

--- 08-install-mySQL.sh
+++ 08-install-mySQL.sh
@@ -7,7 +7,29 @@ if [ $USERID -ne 0 ]
 
 then
    echo "error: please run this script with root access"
-
+   exit 1
 fi
 
-yum install mySQL -y
+yum install mysql -y
+
+if [ $? -ne 0 ]
+
+then
+   echo "installation of mysql is error"
+   exit 1
+
+else
+   echo "installation of mysql is success"
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+
+then
+   echo "installation of mostfix is error"
+   exit 1
+
+else
+   echo "installation of mostfix is success"
+fi
