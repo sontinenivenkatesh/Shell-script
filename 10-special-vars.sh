2ab1e760fae86eafbf3e298042a0a3f50915c951
venkatesh <ssvenkatesh908@gmail.com>
2023-11-17 07:45:40 +0530

    special-variables

---
+++ 10-special-vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#here $0,$@,$# all this special variables
+#here '0' this for script name
+#here '@' this for all variables
+#here '#' this for number of varables
+
+VAR1=$1
+VAR2=$2
+
+echo "script name= $0"
+
+echo "variable1 =$1"
+
+echo "all variables =$@"
+
+echo "Number of variables =$#"
