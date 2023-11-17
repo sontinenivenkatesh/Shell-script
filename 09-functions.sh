4fea2c08d68d1a632c761885744ddc52076000a0
venkatesh <ssvenkatesh908@gmail.com>
2023-11-17 08:05:50 +0530

    special-variables

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-USERID=$(id -u)
-# this function should validate the previous command and inform user it is success or not
+
+DATE=(DATE +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME.$DATE.log
 
 validate(){
 #$1 -- it will receive the arguments
@@ -18,16 +20,19 @@ fi
 
 }
 
+USERID=$(id -u)
+# this function should validate the previous command and inform user it is success or not
+
 if [ $USERID -ne 0 ]
  
 then
    echo "please run this script with root access"
    exit 1
 fi
-   yum install mysql -y
+   yum install mysql -y  &>>$LOGFILE
 
    validate $? "installing mysql"
 
-   yum install postfix -y
+   yum install postfix -y &>>$LOGFILE
 
    validate $? "installing postfix"
