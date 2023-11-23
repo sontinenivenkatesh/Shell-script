6fe7324fa152b3bf6590e81308ac9300f7da1786
venkatesh <ssvenkatesh908@gmail.com>
2023-11-23 13:21:17 +0530

    loops

---
+++ 12-delet-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/APP-LOGS
+DATE=( date +%F )
+LOGSDIR=/home/centos/shellscript.los
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
+
