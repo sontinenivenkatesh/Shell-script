fcb022c322c4f72556cee619f22ef2bd31d41048
venkatesh <ssvenkatesh908@gmail.com>
2023-12-08 07:50:46 +0530

    delete_log_files

--- 12-delet-old-logs.sh
+++ 12-delet-old-logs.sh
@@ -2,7 +2,7 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 DATE=$( date +%F )
-LOGSDIR=/home/centos/shellscript.los
+LOGSDIR=/home/centos/shellscript.logs
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 
